628a686d7b546180eb2ae288c6222f6eef19f877
Sarah Naas <sjnaas@gmail.com>
2019-12-11 13:26:08 -0800

    Display the data needed. added some functions to begin playing with the data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MySQL-Project
+
+To get this app to work you will need to install the following npms:
+-inquirer
+-mysql
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,8 @@
 const mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var myItems = [];
+var selectedItem = [];
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -11,4 +15,66 @@ const connection = mysql.createConnection({
 connection.connect(err => {
   if (err) throw err;
   console.log("Connection set up! Woohoo!");
+  seeAllProducts();
+  connection.end();
 });
+
+const seeAllProducts = () => {
+  connection.query("SELECT * FROM products", (err, res) => {
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {
+      var items = res[i];
+      myItems.push(items);
+      console.log(
+        "ID: " +
+          res[i].id +
+          "  Item: " +
+          res[i].product_name +
+          "  $" +
+          res[i].price
+      );
+    }
+    console.log(myItems);
+    selectID();
+  });
+};
+
+const selectID = () => {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "Please enter the product ID",
+      name: "productID"
+    })
+    .then(res => {
+      var ID = res.productID;
+      for (var i = 0; i < myItems.length; i++) {
+        if (ID == myItems[i].id) {
+          selectedItem.push(myItems[i]);
+        }
+      }
+      orderQuantity();
+    });
+};
+
+const orderQuantity = () => {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "Select how many you would like to purchase",
+      name: "quantity"
+    })
+    .then(res => {
+      console.log(res.quantity);
+      //if I do it this way does it mean that I can't update the database?
+    });
+};
+
+//find the stock quantity that corresponds to the id#
+//if there are not enough in stock
+//console.log Not enough in stock
+//else
+//find the price of the item associated with the selected product ID.
+// multiply price by quanitity
+//update the database with the new quantity
