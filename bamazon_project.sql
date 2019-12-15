f8b79db5a691b408dd687a3e5f6d15cb2d165368
Sarah Naas <sjnaas@gmail.com>
2019-12-14 23:45:40 -0800

    added photos to read me and SQL file

--- bamazon_project.sql
+++ bamazon_project.sql
@@ -1,28 +1,13 @@
-# MySQL-Project
-
-This app is built as an exercise in building a MySQL database and manipulationg it with Node.js
-
-Technologies used include:
-Node.js, Javascript, MySQL
-
-You will need to install the following NPMs to get the app to work:
--inquirer
--mysql
-
-To make this App I created a table on MySQL. I then created a connection between my app and mySQL so I could access the table using Node.js.
-
-Here is the MySQL code:
-
 DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 USE bamazon_db;
 CREATE TABLE products(
-id INTEGER AUTO_INCREMENT NOT NULL,
-product_name VARCHAR(140),
-department_name VARCHAR(140),
-price INTEGER,
-stock_quantity INTEGER,
-PRIMARY KEY(id, product_name, department_name, price, stock_quantity)
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(140),
+    department_name VARCHAR(140),
+    price INTEGER,
+    stock_quantity INTEGER,
+    PRIMARY KEY(id, product_name, department_name, price, stock_quantity)
 );
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
@@ -33,7 +18,7 @@ VALUES('Pants', 'Clothing', 50, 40);
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
 VALUES('Computer', 'Electronics', 350, 50);
-
+    
 INSERT INTO products(product_name, department_name, price, stock_quantity)
 VALUES('Television', 'Electronics', 400, 30);
 
@@ -42,7 +27,7 @@ VALUES('Ashtrays', 'Books', 20, 35);
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
 VALUES('Kye', 'Books', 20, 30);
-
+ 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
 VALUES('Coat', 'Clothing', 100, 45);
 
@@ -55,18 +40,5 @@ VALUES('Couch', 'Home & Decor', 700, 32);
 INSERT INTO products(product_name, department_name, price, stock_quantity)
 VALUES('Table', 'Home & Decor', 550, 38);
 
-SELECT \* FROM products;
 
----
-
-How the App works:
-
-When you run the app the list of products will appear with their ID numbers. Inquirer will then prompt the user to select the product ID of the item they would like to purchase.
-
-![GitHub Logo](/images/screenshot1.png)
-
-Then, they will be asked how many of said article they would like to buy. The total price is then calculated and the mySQL database is updated with the remaining stock.
-
-![GitHub Logo](/images/screenshot2.png)
-
-![GitHub Logo](/images/screenshot3.png)
+SELECT * FROM products;
